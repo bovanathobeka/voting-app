c5cf833fe55dc2abd9d5cbf890ef101a0d79d4e5
Thobeka Bovava <bovanathobeka@gmail.com>
2025-06-26 12:28:26 +1400

    diploying firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,46 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import Stripe from 'stripe';
 
+admin.initializeApp();
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const stripe = new Stripe(functions.config().stripe.secret, {
+//   apiVersion: '2022-11-15',
+});
 
+export const createPaymentIntent = functions.https.onRequest(async (req, res) => {
+  try {
+    const { amount, currency } = req.body;
 
+    if (!amount || !currency) {
+      res.status(400).json({ error: 'Missing amount or currency' });
+      return;
+    }
+
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: '2022-11-15' }
+    );
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+      customer: customer.id,
+    });
+
+    
+    console.log('client_secret:', paymentIntent.client_secret);
+    console.log('ephemeralKey:', ephemeralKey.secret);
+    console.log('customer:', customer.id);
+
+      res.status(200).json({
+      clientSecret: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customerId: customer.id,
+    });
+  } catch (error: any) {
+    console.error('Error creating payment intent:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
